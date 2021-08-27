68f5c0561229e213f687b63d2b626cf57ea0d6be
thiagoboneth <thiago.luis@mercadolivre.com>
2021-08-27 14:06:33 -0300

    Implementado funcao de saque

--- Conta.java
+++ Conta.java
@@ -3,4 +3,18 @@ public class Conta {
     int agencia;
     int numero;
     String titular;
+
+    public void depositar(double valor){
+        this.saldo += valor;
+    }
+    public boolean sacar(double valorSaque){
+        if (this.saldo >= valorSaque){
+            System.out.println("Saque efetuado com sucesso no valor de $" + valorSaque);
+            this.saldo -= valorSaque;
+            return true;
+        } else {
+            System.out.println("Saldo indisponivel para saque, saque disponivel no valor de $" + this.saldo + " reais");
+            return false;
+        }
+    }
 }
--- CriaConta.java
+++ CriaConta.java
@@ -6,6 +6,13 @@ public class CriaConta {
         primeiraConta.numero = 304050;
         primeiraConta.saldo = 1000.00;
 
+        primeiraConta.depositar(1000);
+
         System.out.println(primeiraConta.titular);
+        System.out.println("Cliente " + primeiraConta.titular + " Saldo " + primeiraConta.saldo);
+
+        primeiraConta.sacar(500);
+
+        System.out.println(primeiraConta.saldo);
     }
 }
